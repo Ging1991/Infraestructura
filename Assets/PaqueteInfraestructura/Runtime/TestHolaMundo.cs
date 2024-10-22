e7964dba2416cabddf13d4c30d29dde53082ea94
Carlos Caballero <carlos_velez10@hotmail.com>
2024-10-22 13:45:04 -0300

    Update TestHolaMundo.cs

--- Assets/PaqueteInfraestructura/Runtime/TestHolaMundo.cs
+++ Assets/PaqueteInfraestructura/Runtime/TestHolaMundo.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class TestHolaMundo : MonoBehaviour {
 
     public static void Saludar(){
-        Debug.Log("Hola mundo!");        
+        Debug.Log("Hola mundo!2");        
     }
 
 }
